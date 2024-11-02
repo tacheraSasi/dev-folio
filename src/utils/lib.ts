9950796824c88a446c7f040425a1e6781a04caaa
tacheraSasi <tacherasasi@gmail.com>
2024-11-02 21:34:34 +0300

    created the  the utils directory and added lib.ts

---
+++ src/utils/lib.ts
@@ -0,0 +1,42 @@
+export const skillCategories = [
+    {
+      title: "Programming languages",
+      skills: [
+        { name: "Go", icon: "https://img.shields.io/badge/-Go-00ADD8?style=for-the-badge&logo=go&logoColor=white" },
+        { name: "PHP", icon: "https://img.shields.io/badge/-PHP-777BB4?style=for-the-badge&logo=php&logoColor=white" },
+        { name: "Python", icon: "https://img.shields.io/badge/-Python-3776AB?style=for-the-badge&logo=python&logoColor=white" },
+        { name: "TypeScript", icon: "https://img.shields.io/badge/-TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" },
+        { name: "Java", icon: "https://img.shields.io/badge/-Java-007396?style=for-the-badge&logo=java&logoColor=white" },
+        { name: "C++", icon: "https://img.shields.io/badge/-C++-00599C?style=for-the-badge&logo=cplusplus&logoColor=white" },
+        { name: "JavaScript", icon: "https://img.shields.io/badge/-JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" },
+      ],
+    },
+    {
+      title: "Frameworks & Tools",
+      skills: [
+        { name: "Django", icon: "https://img.shields.io/badge/-Django-092E20?style=for-the-badge&logo=django&logoColor=white" },
+        { name: "React", icon: "https://img.shields.io/badge/-React-61DAFB?style=for-the-badge&logo=react&logoColor=black" },
+        { name: "Node.js", icon: "https://img.shields.io/badge/-Node.js-339933?style=for-the-badge&logo=node.js&logoColor=white" },
+        { name: "Next.js", icon: "https://img.shields.io/badge/-Next.js-000000?style=for-the-badge&logo=next.js&logoColor=white" },
+        { name: "React Native", icon: "https://img.shields.io/badge/-React_Native-61DAFB?style=for-the-badge&logo=react&logoColor=black" },
+        { name: "PostgreSQL", icon: "https://img.shields.io/badge/-PostgreSQL-4169E1?style=for-the-badge&logo=postgresql&logoColor=white" },
+        { name: "Docker", icon: "https://img.shields.io/badge/-Docker-2496ED?style=for-the-badge&logo=docker&logoColor=white" },
+      ],
+    },
+    {
+      title: "Backend Frameworks",
+      skills: [
+        { name: "Gin", icon: "https://img.shields.io/badge/-Gin-007396?style=for-the-badge&logo=gin&logoColor=white" },
+        { name: "Fiber", icon: "https://img.shields.io/badge/-Fiber-00C7B7?style=for-the-badge&logo=gofiber&logoColor=white" },
+        { name: "Express.js", icon: "https://img.shields.io/badge/-Express.js-000000?style=for-the-badge&logo=express&logoColor=white" },
+        { name: "Hono", icon: "https://img.shields.io/badge/-Hono-FF5722?style=for-the-badge&logo=hono&logoColor=white" },
+      ],
+    },
+];
+
+export const devEnvironment = [
+  { name: "OS", value: "Ubuntu 20.04 LTS, GNOME" },
+  { name: "Editor", value: "VSCode, NeoVim" },
+  { name: "Databases", value: "PostgreSQL, MySQL, SQLite" },
+  { name: "Other Tools", value: "Git, Prisma, HTMX" },
+];
